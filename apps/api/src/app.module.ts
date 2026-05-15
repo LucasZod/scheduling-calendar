bd9cfca188bc7e9c15b7d37c942055d7ec4e220c
Lucas Moreno <lucas@hyperdev.tech>
2026-05-15 16:28:51 -0300

    feat(aoi): config module && pipe

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { validateEnv } from './config/env'
+import { DatabaseModule } from './database/database.module'
+import { SchedulingsModule } from './schedulings/schedulings.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnv,
+    }),
+    DatabaseModule,
+    SchedulingsModule,
+  ],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import type { Env } from './config/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
+
+  const config = app.get(ConfigService<Env, true>)
+  app.enableCors({ origin: config.getOrThrow('CORS_ORIGIN', { infer: true }) })
+
+  await app.listen(config.getOrThrow('PORT', { infer: true }))
 }
-bootstrap()
+
+void bootstrap()
